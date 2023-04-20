a6238f715b82da72a60fb49f183492ac5312e8a7
Rob B <rob@rjrobbie.dev>
2023-04-20 16:06:05 +0100

    remove boiler plate code

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../styles/app.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/navbar.css"
+
+function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
